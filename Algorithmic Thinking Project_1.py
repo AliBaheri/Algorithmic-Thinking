6fa376882e7cb92570e851305621a2e5a48eb0da
Ali_Baheri <ali.baheri4614@gmail.com>
2015-06-06 23:20:31 -0500

    Project_1
    Building a competed directed graph and …

---
+++ Algorithmic Thinking Project_1.py
@@ -0,0 +1,45 @@
+"""
+Algorithmic thinking - Project 1 - Ali Baheri
+"""
+EX_GRAPH0 = {0: set([1, 2]), 1: set([]), 2: set([])}
+EX_GRAPH1 = {0: set([1, 4, 5]), 1: set([2, 6]), 2: set([3]), 3: set([0]), 4: set([1]),
+             5: set([2]), 6: set([])}
+EX_GRAPH2 = {0: set([1, 4, 5]), 1: set([2, 6]), 2: set([3, 7]), 3: set([7]), 4: set([1]),
+             5: set([2]), 6: set([]), 7: set([3]), 8: set([1, 2]), 9: set([0, 3, 4, 5, 6, 7])}
+"""
+bulding a complete directed graph
+"""
+def make_complete_graph(num_nodes):
+    complete_graph = {}
+    set_nodes = set(range(num_nodes))
+    
+    for node in range(num_nodes):
+        complete_graph[node] = set_nodes - set([node])
+
+    return complete_graph
+
+"""
+calculating in-degree of nodes
+"""
+def compute_in_degrees(digraph):
+    in_degree = {}
+    for key in digraph:
+        in_degree[key] = 0
+    for key in digraph:
+        for dist in digraph[key]:
+            in_degree[dist] += 1
+    return in_degree
+
+"""
+calculating in-degree distribution
+"""
+def in_degree_distribution(digraph):
+    in_degree = compute_in_degrees(digraph)
+    distribution = {}
+    for key in in_degree:
+        length = in_degree[key]
+        if length in distribution:
+            distribution[length] += 1
+        else:
+            distribution[length] = 1
+    return distribution
